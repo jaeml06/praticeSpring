b0c08a46163210e173cdb8d8399a570ea056e5da
jaeml00 <jaeml0630@gmail.com>
2023-08-10 16:41:15 +0900

    FEAT : JSON 파싱 기능 추가

--- druginfo/src/main/java/com/dcm/spring/druginfo/controller/DrugOpenApiController.java
+++ druginfo/src/main/java/com/dcm/spring/druginfo/controller/DrugOpenApiController.java
@@ -1,5 +1,6 @@
 package com.dcm.spring.druginfo.controller;
 
+import com.dcm.spring.druginfo.model.DrugResponseDto;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,17 +17,22 @@ public class DrugOpenApiController {
     private final static String BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList";
     private final String API_KEY = "LBfaHapjnwmhChga%2FgsL2yJJUiy7YQ9hH%2Bq%2FEP9pJ9OBS6v%2B8VUYyJUoWnRV8b9nz3NI%2F7Rq35MPx5NvqGj8CQ%3D%3D";
 
+    private Mono<Object> convertToMedication(DrugResponseDto drugResponseDTO) {
+        return Mono.just(drugResponseDTO.getBody().getItems().get(0));
+    }
+
     @GetMapping(value = "/PublicData/{itemName}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> getDrugInfo(@PathVariable String itemName) {
+    public Mono<Object> getDrugInfo(@PathVariable String itemName) {
         DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(BASE_URL);
         factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+
         String encodedItemName = URLEncoder.encode(itemName, StandardCharsets.UTF_8);
         WebClient webClient = WebClient.builder()
                 .uriBuilderFactory(factory)
                 .baseUrl(BASE_URL)
                 .build();
 
-        Mono<String> response = webClient.get()
+        Mono<DrugResponseDto> response = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .queryParam("serviceKey", API_KEY)
                         .queryParam("pageNo", "1")
@@ -35,8 +41,8 @@ public class DrugOpenApiController {
                         .queryParam("type", "json")
                         .build())
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToMono(DrugResponseDto.class);
 
-        return response;
+        return response.flatMap(this::convertToMedication);
     }
 }
